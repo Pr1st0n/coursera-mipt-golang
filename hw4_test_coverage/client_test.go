93481e85b5dbcef33dd404fa2a6f37eb979737e3
Pr1st0n <k0t9p@yahoo.com>
2019-08-09 19:29:52 +0200

    Implemented base logic for ServerSearch function and empty request test for hw4.

--- hw4_test_coverage/client_test.go
+++ hw4_test_coverage/client_test.go
@@ -1,3 +1,93 @@
 package main
 
+import (
+	"encoding/json"
+	"encoding/xml"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"reflect"
+	"testing"
+)
+
+type UsersXml struct {
+	Version string    `xml:"version,attr"`
+	List    []UserXml `xml:"row"`
+}
+
+type UserXml struct {
+	Id        int    `xml:"id"`
+	FirstName string `xml:"first_name"`
+	LastName  string `xml:"last_name"`
+	Age       int    `xml:"age"`
+	About     string `xml:"about"`
+	Gender    string `xml:"gender"`
+}
+
 // код писать тут
+func SearchServer(res http.ResponseWriter, req *http.Request) {
+	users, err := getUsers()
+	if err != nil {
+		fmt.Printf("SearchServer error: %v", err)
+		return
+	}
+
+	usersJson, err := json.Marshal(users)
+	if err != nil {
+		fmt.Printf("SearchServer error: %v", err)
+		return
+	}
+
+	res.WriteHeader(http.StatusOK)
+	_, err = res.Write(usersJson)
+	if err != nil {
+		fmt.Printf("SearchServer error: %v", err)
+		return
+	}
+}
+
+func getUsers() ([]User, error) {
+	fileReader, err := os.Open("./dataset.xml")
+	if err != nil {
+		return nil, err
+	}
+	defer fileReader.Close()
+	bytes, err := ioutil.ReadAll(fileReader)
+	if err != nil {
+		return nil, err
+	}
+
+	usersXml := UsersXml{}
+	xml.Unmarshal(bytes, &usersXml)
+	users := make([]User, len(usersXml.List))
+
+	for idx, userXml := range usersXml.List {
+		users[idx] = User{
+			Id:     userXml.Id,
+			Name:   userXml.FirstName + " " + userXml.LastName,
+			Age:    userXml.Age,
+			About:  userXml.About,
+			Gender: userXml.Gender,
+		}
+	}
+
+	return users, nil
+}
+
+func TestEmptyRequest(t *testing.T) {
+	// Initialize test server instance
+	testServer := httptest.NewServer(http.HandlerFunc(SearchServer))
+	defer testServer.Close()
+	users, _ := getUsers()
+	expected := &SearchResponse{Users: users, NextPage: false}
+	client := SearchClient{AccessToken: "test_token", URL: testServer.URL}
+	request := SearchRequest{}
+	result, err := client.FindUsers(request)
+	if err != nil {
+		t.Errorf("FindUsers error: %v", err)
+	} else if !reflect.DeepEqual(expected, result) {
+		t.Errorf("Expected: %v\nActual: %v", expected, result)
+	}
+}
